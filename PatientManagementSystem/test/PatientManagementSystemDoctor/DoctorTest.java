ecf77f2180b5524d148673490ff4673ed0b4f3e7
leecadman <leecadman@hotmail.com>
2020-01-08 14:46:08 +0000

    JUNIT tests for each class

---
+++ PatientManagementSystem/test/PatientManagementSystemDoctor/DoctorTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PatientManagementSystemDoctor;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Lee
+ */
+public class DoctorTest {
+    
+    public DoctorTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class Doctor.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        Doctor.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+      
+    }
+    
+}
